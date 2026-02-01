6cdff1cc771095e46854cf2a5fd5da58cf39445b
omeruren <omer.uren19@gmail.com>
2026-02-01 15:04:31 +0300

    implemented Domain event samples

---
+++ DomainDrivenDesign.Domain/Orders/Events/SendOrderEmailEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace DomainDrivenDesign.Domain.Orders.Events;
+public sealed class SendOrderEmailEvent : INotificationHandler<OrderDomainEvent>
+{
+    public Task Handle(OrderDomainEvent notification, CancellationToken cancellationToken)
+    {
+        // send email event
+        return Task.CompletedTask;
+    }
+}
---
+++ DomainDrivenDesign.Domain/Users/Events/SendRegisterEmailEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace DomainDrivenDesign.Domain.Users.Events;
+public sealed class SendRegisterEmailEvent : INotificationHandler<UserDomainEvent>
+{
+    public Task Handle(UserDomainEvent notification, CancellationToken cancellationToken)
+    {
+        // send notification
+        return Task.CompletedTask;
+    }
+}
